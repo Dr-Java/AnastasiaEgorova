3564fdffdb32779e3b0137de499d246b450c37e6
Anastasiia Egorova <Anastasiia_Egorova1@epam.com>
2021-08-02 15:00:12 +0300

    hw1 completed

--- src/test/java/com/epam/tc/egorova/hw1/LongMultTest.java
+++ src/test/java/com/epam/tc/egorova/hw1/LongMultTest.java
@@ -1,2 +1,27 @@
-package com.epam.tc.egorova.hw1;public class LongMultTest {
+package com.epam.tc.egorova.hw1;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LongMultTest extends CalculatorBaseTests {
+
+    @Test(dataProvider = "longMultTestDataProvider", groups = {"multdiv"})
+    public void testLongMult(long a, long b, long expectedResult) {
+        long actualResult = calculator.mult(a, b);
+        AssertionsForClassTypes.assertThat(actualResult)
+                               .as("Unexpected multiplication result")
+                               .isEqualTo(expectedResult);
+    }
+
+    @DataProvider(name = "longMultTestDataProvider")
+    public Object[][] longMultTestDataProvider() {
+        return new Object[][]{
+            {4, 6, 24},
+            {5, 0, 0},
+            {-20, 4, -80},
+            {-3, -6, 18}
+        };
+    }
 }
+
--- src/test/java/com/epam/tc/egorova/hw1/LongSumTest.java
+++ src/test/java/com/epam/tc/egorova/hw1/LongSumTest.java
@@ -1,2 +1,26 @@
-package com.epam.tc.egorova.hw1;public class LongSumTest {
+package com.epam.tc.egorova.hw1;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LongSumTest extends CalculatorBaseTests {
+
+    @Test(dataProvider = "longSumTestDataProvider", groups = {"default"})
+    public void testLongSum(long a, long b, long expectedResult) {
+        long actualResult = calculator.sum(a, b);
+        AssertionsForClassTypes.assertThat(actualResult)
+                               .as("Unexpected summation result")
+                               .isEqualTo(expectedResult);
+    }
+
+    @DataProvider(name = "longSumTestDataProvider")
+    public Object[][] longSumTestDataProvider() {
+        return new Object[][]{
+            {10, 20, 30},
+            {2, 2, 4},
+            {-150, 150, 0},
+            {0, -200, -200}
+        };
+    }
 }
