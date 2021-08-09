a6f0130648d1c9b0ab301d38a32d4c09f7694a0a
Anastasiia Egorova <Anastasiia_Egorova1@epam.com>
2021-08-09 14:05:08 +0300

    hw2 fixed

--- src/test/java/com/epam/tc/egorova/hw2/ex1/ExerciseOneTest.java
+++ src/test/java/com/epam/tc/egorova/hw2/ex1/ExerciseOneTest.java
@@ -1,6 +1,7 @@
 package com.epam.tc.egorova.hw2.ex1;
 
 import com.epam.tc.egorova.hw2.BaseTest;
+import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
@@ -49,75 +50,59 @@ public class ExerciseOneTest extends BaseTest {
         //Assert that there are 4 items on the header section are displayed and they have proper texts
         List<WebElement> headerItems = driver.findElements(By
             .xpath("//ul[contains(@class, 'm-l8')]/li"));
+
         int expectedHeaderItemsNumber = 4;
         softAssert.assertThat(headerItems.size())
                   .as("Header items number is incorrect")
                   .isEqualTo(expectedHeaderItemsNumber);
+
+        List<String> expectedHeaderItems = Arrays
+            .asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS");
         for (WebElement item : headerItems) {
             softAssert.assertThat(item.isDisplayed())
                       .as("Items are not displayed");
         }
 
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='uui-navigation nav navbar-nav m-l8']//a[@href='index.html']")).getText())
-                  .as("Header item text is incorrect")
-                  .isEqualTo("HOME");
-
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='uui-navigation nav navbar-nav m-l8']//a[@href='contacts.html']")).getText())
-                  .as("Header item text is incorrect")
-                  .isEqualTo("CONTACT FORM");
-
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='uui-navigation nav navbar-nav m-l8']//a[@class='dropdown-toggle']")).getText())
-                  .as("Header item text is incorrect")
-                  .isEqualTo("SERVICE");
-
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='uui-navigation nav navbar-nav m-l8']//a[@href='metals-colors.html']")).getText())
-                  .as("Header item text is incorrect")
-                  .isEqualTo("METALS & COLORS");
+        for (int i = 0; i < headerItems.size(); i++) {
+            softAssert.assertThat(headerItems.get(i).getText())
+                      .as("Header item text is incorrect")
+                      .isEqualTo(expectedHeaderItems.get(i));
+        }
 
         //Assert that there are 4 images on the Index Page and they are displayed
         List<WebElement> indexImages = driver.findElements(By.className("benefit-icon"));
+
         int expectedIndexImagesNumber = 4;
         softAssert.assertThat(indexImages.size())
                   .as("Images number is incorrect")
                   .isEqualTo(expectedIndexImagesNumber);
+
         for (WebElement image : indexImages) {
             softAssert.assertThat(image.isDisplayed()).as("Images are not displayed");
         }
 
         //Assert that there are 4 texts on the Index Page under icons and they have proper text
         List<WebElement> indexTextsUnderIcons = driver.findElements(By.xpath("//span[@class='benefit-txt']"));
-        int expectedIndexTextsUnderIcons = 4;
+
+        int expectedIndexTextsUnderIconsNumber = 4;
         softAssert.assertThat(indexTextsUnderIcons.size())
                   .as("Texts number is incorrect")
-                  .isEqualTo(expectedIndexTextsUnderIcons);
+                  .isEqualTo(expectedIndexTextsUnderIconsNumber);
 
-        String expectedFirstIndexText = "To include good practices\n"
-            + "and ideas from successful\n"
-            + "EPAM project";
-        softAssert.assertThat(driver.findElement(By.xpath("(//span[@class='benefit-txt'])[1]")).getText())
-                  .as("Text is incorrect")
-                  .isEqualTo(expectedFirstIndexText);
+        List<String> expectedIndexTextsUnderIcons = Arrays
+            .asList("To include good practices\n"
+                + "and ideas from successful\n"
+                + "EPAM project", "To be flexible and\n"
+                + "customizable", "To be multiplatform", "Already have good base\n"
+                + "(about 20 internal and\n"
+                + "some external projects),\n"
+                + "wish to get more…");
 
-        String expectedSecondIndexText = "To be flexible and\n"
-            + "customizable";
-        softAssert.assertThat(driver.findElement(By.xpath("(//span[@class='benefit-txt'])[2]")).getText())
-                  .as("Text is incorrect").isEqualTo(expectedSecondIndexText);
-
-        String expectedThirdIndexText = "To be multiplatform";
-        softAssert.assertThat(driver.findElement(By.xpath("(//span[@class='benefit-txt'])[3]")).getText())
-                  .as("Text is incorrect")
-                  .isEqualTo(expectedThirdIndexText);
-
-        String expectedFourthIndexText = "Already have good base\n"
-            + "(about 20 internal and\n"
-            + "some external projects),\n"
-            + "wish to get more…";
-        softAssert.assertThat(driver.findElement(By.xpath("(//span[@class='benefit-txt'])[4]")).getText())
-                  .as("Text is incorrect").isEqualTo(expectedFourthIndexText);
+        for (int i = 0; i < indexTextsUnderIcons.size(); i++) {
+            softAssert.assertThat(indexTextsUnderIcons.get(i).getText())
+                      .as("Text is incorrect")
+                      .isEqualTo(expectedIndexTextsUnderIcons.get(i));
+        }
 
         //Assert that there is the iframe with “Frame Button” exist
         softAssert.assertThat(driver.findElement(By.xpath("//iframe[@id='frame']")).isDisplayed())
@@ -135,39 +120,25 @@ public class ExerciseOneTest extends BaseTest {
         //Assert that there are 5 items in the Left Section are displayed and they have proper text
         List<WebElement> leftSectionItems = driver.findElements(By
             .xpath("//ul[contains(@class,'left')]/li/a"));
-        int expectedLeftSectionItems = 5;
+
+        int expectedLeftSectionItemsNumber = 5;
         softAssert.assertThat(leftSectionItems.size())
                   .as("Left Section items number is incorrect")
-                  .isEqualTo(expectedLeftSectionItems);
+                  .isEqualTo(expectedLeftSectionItemsNumber);
+
+        List<String> expectedLeftSectionItems = Arrays
+            .asList("Home", "Contact form", "Service", "Metals & Colors", "Elements packs");
+
         for (WebElement item : leftSectionItems) {
             softAssert.assertThat(item.isDisplayed())
                       .as("Left Section items are not displayed");
         }
 
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='sidebar-menu left']//a[@href='index.html']")).getText())
-                  .as("Text is incorrect")
-                  .isEqualTo("Home");
-
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='sidebar-menu left']//a[@href='contacts.html']")).getText())
-                  .as("Text is incorrect")
-                  .isEqualTo("Contact form");
-
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='sidebar-menu left']//span[text()='Service']")).getText())
-                  .as("Text is incorrect")
-                  .isEqualTo("Service");
-
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='sidebar-menu left']//a[@href='metals-colors.html']")).getText())
-                  .as("Text is incorrect")
-                  .isEqualTo("Metals & Colors");
-
-        softAssert.assertThat(driver.findElement(By
-            .xpath("//ul[@class='sidebar-menu left']//span[text()='Elements packs']")).getText())
-                  .as("Text is incorrect")
-                  .isEqualTo("Elements packs");
+        for (int i = 0; i < indexTextsUnderIcons.size(); i++) {
+            softAssert.assertThat(leftSectionItems.get(i).getText())
+                      .as("Text is incorrect")
+                      .isEqualTo(expectedLeftSectionItems.get(i));
+        }
 
         softAssert.assertAll();
     }
