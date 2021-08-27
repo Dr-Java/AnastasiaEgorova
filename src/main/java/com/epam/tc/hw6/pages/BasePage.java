b391f3dcfbf55e33d41edaa0871892f53d8897e5
Anastasiia Egorova <Anastasiia_Egorova1@epam.com>
2021-08-27 09:57:26 +0300

    homework6 completed

---
+++ src/main/java/com/epam/tc/hw6/pages/BasePage.java
@@ -0,0 +1,18 @@
+package com.epam.tc.hw6.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getBrowserTitle() {
+        return driver.getTitle();
+    }
+}
